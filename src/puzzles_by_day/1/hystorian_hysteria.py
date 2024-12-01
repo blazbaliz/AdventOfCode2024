05fdc3f4b4421e3b2d20189c8f6b2e3251dba5a3
blaz.baliz <blaz.baliz@gmail.com>
2024-12-01 23:45:39 +0100

    minor bugfix

---
+++ src/puzzles_by_day/1/hystorian_hysteria.py
@@ -0,0 +1,22 @@
+class HystorianHysteria():
+
+    _first_list = []
+    _second_list = []
+
+    def __init__(self):
+        with open('puzzle_input.txt', 'r') as file:
+            for line in file.readlines():
+                rows = line.split()
+                self._first_list.append(rows[0])
+                self._second_list.append(rows[1])
+                
+        self._first_list = sorted(self._first_list, key=int)
+        self._second_list = sorted(self._second_list, key=int)
+
+    def do_puzzle(self):
+        distance = 0
+
+        for id in range(len(self._first_list)):
+            distance += int(self._second_list[id]) - int(self._first_list[id]) 
+
+        return distance
